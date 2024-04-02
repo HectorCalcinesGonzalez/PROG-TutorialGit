b0fb3de663ef15b1a0b1ece02d4b7b621ad63760
HectorCalcinesGonzalez <159430801+HectorCalcinesGonzalez@users.noreply.github.com>
2024-04-02 12:21:09 +0100

    feat(Main): ask number to user and print it

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("¡¡Hola Mundo!!");
+        System.out.println("¡¡Hola Alumno!!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduzca un numerito");
+        int askedNumber = scanner.nextInt();
+
+        System.out.println("El numero seleccionado es: " + askedNumber);
+
+        scanner.close();
+
     }
 }
